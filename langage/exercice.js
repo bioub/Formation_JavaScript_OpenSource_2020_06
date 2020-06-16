e4c64c161434941acccf78a33ef774e60a8bcb65
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-06-16 15:47:42 +0200

    Correction

--- langage/exercice.js
+++ langage/exercice.js
@@ -6,17 +6,34 @@ const rl = readline.createInterface({
 });
 
 function jouer() {
+  console.log('Vous avez saisi : ' + essais.join(' - '));
   rl.question('Quel est le nombre ? ', (saisie) => {
     // saisie est de type string
+    const entierSaisi = parseInt(saisie);
 
-    // pour rejouer
-    jouer();
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un nombre');
+      return jouer();
+    }
 
-    // pour arrêter la partie
-    // rl.close();
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
+
   });
 }
 
+const entierAlea = Math.floor(Math.random() * 101);
+const essais = [];
 jouer();
 
 
